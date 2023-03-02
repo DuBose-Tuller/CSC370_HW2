ad23c7129d3a3ecd80f5e97600b9add018899d7f
DuBose Tuller <d.tuller@wustl.edu>
2023-03-02 13:40:50 -0500

    mutation and crossover

--- Equation.py
+++ Equation.py
@@ -12,14 +12,14 @@ class Equation:
         assert params["two_operators"] >= params["one_operator"]
         
         if params["is_real"]:
-            randval = random.uniform(params["min_val"], params["max_val"])
+            randval = random.uniform
         else:
-            randval = random.randint(params["min_val"], params["max_val"])
+            randval = random.randint
         
         
         # Set root
         queue = []
-        root = Node(random.choice(operators), 0)
+        root = Node(random.choice(operators), params["start_depth"])
         self.set_root(root)
         queue.append(root)
 
@@ -27,14 +27,14 @@ class Equation:
             cur_node = queue.pop()
 
             ops_check = random.random()
-            right = ""
-            left  = ""
-            if cur_node.depth == params["max_depth"] or ops_check > params["two_operators"]:
+            # right = ""
+            # left  = ""
+            if cur_node.depth == params["max_depth"]  or ops_check > params["two_operators"]:
                 # Generate two val children
-                right = Node(randval, cur_node.depth + 1)
+                right = self.generate_val_node(params, cur_node.depth + 1, randval)
                 self.nodes.append(right)
                 right.set_parent(cur_node)
-                left  = Node(randval, cur_node.depth + 1)
+                left  = self.generate_val_node(params, cur_node.depth + 1, randval)
                 self.nodes.append(left)
                 left.set_parent(cur_node)
 
@@ -48,7 +48,7 @@ class Equation:
                     right.set_parent(cur_node)
                     queue.append(right)
 
-                    left  = Node(randval, cur_node.depth + 1)
+                    left  = self.generate_val_node(params, cur_node.depth + 1, randval)
                     self.nodes.append(left)
                     left.set_parent(cur_node)
                     
@@ -58,7 +58,7 @@ class Equation:
                     left.set_parent(cur_node)
                     queue.append(left)
 
-                    right  = Node(randval, cur_node.depth + 1)
+                    right  = self.generate_val_node(params, cur_node.depth + 1, randval)
                     self.nodes.append(right)
                     right.set_parent(cur_node)
 
@@ -78,16 +78,63 @@ class Equation:
             cur_node.set_left(left)
             cur_node.set_right(right)
         
-        
+
+    def generate_val_node(self, params, depth, randval):
+        x_check = random.random()
+
+        if x_check < params["val_is_x"]:
+            node = Node("x", depth)
+        else: 
+            node = Node(randval(params["min_val"], params["max_val"]), depth)
+
+        return node
+     
     def mutate(self, params):
         # Get a list of all the value nodes, and change a random one
         equation = self.copy() # deep copy the tree
-        mutation_point = equation.randNode()
+        mutation_point = random.choice(equation.nodes)
+
+        start_depth = mutation_point.depth
+        params["start_depth"] = start_depth
+
         new_subtree = Equation(params)
+        parent = mutation_point.parent
+        new_subtree.parent = parent
+
+        if parent.left == mutation_point:
+            parent.left = new_subtree.root
+        else:
+            parent.right = new_subtree.root
+
+        return equation
 
 
-    def crossover(self):
-        pass
+    def crossover(self, other):
+        assert(type(other) == Equation)
+
+        child1 = self.copy()
+        child2 = other.copy()
+
+        child1_cross = random.choice(child1.nodes)
+        child2_cross = random.choice(child2.nodes)
+
+        parent1 = child1_cross.parent
+        parent2 = child2_cross.parent
+        child2_cross.parent = parent1
+        child1_cross.parent = parent2
+
+        if parent1.left == child1_cross:
+            parent1.left = child2_cross
+        else:
+            parent1.right = child2_cross
+
+        if parent2.left == child2_cross:
+            parent2.left = child1_cross
+        else:
+            parent2.right = child1_cross
+
+        return [child1, child2]
+
 
     def set_root(self, Node):
         self.root = Node
@@ -127,7 +174,7 @@ class Equation:
         total_error = 0
 
         for (x, y) in zip(xs, ys):
-            error = y - self.evaluate(x)
+            error = y - self.evaluate(x, self.root)
             total_error += error**2
 
         return total_error/len(x)
--- test.py
+++ test.py
@@ -4,19 +4,18 @@ from Equation import *
 params = {
     "one_operator": 0.3,
     "two_operators": 0.6,
-    "val_is_x": 0.25,
+    "val_is_x": 0.4,
     "min_val": -5,
     "max_val": 5,
-    "max_depth": 3,
-    "is_real": False
+    "max_depth": 1,
+    "is_real": False,
+    "start_depth": 0,
 }
 
-def initialize(size, is_real, max_depth, threshold):
+def initialize(size, params):
     return [Equation(params) for i in range(size)]
 
-eqn = Equation(params)
-print(len(eqn.nodes))
-eqn2 = eqn.copy()
+first_gen = initialize(2, params)
+next_gen = first_gen[0].crossover(first_gen[1])
 
-eqn2.root.left.value = 5
 print("")
