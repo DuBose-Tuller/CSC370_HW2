e092c8b033d321ef6e0f44d4172bc579bce6a9fb
DuBose Tuller <dutuller@davidson.edu>
2023-03-17 18:31:43 -0400

    next gen now selected by tournament

--- Experimentation.py
+++ Experimentation.py
@@ -14,8 +14,8 @@ new_eqn_params = {
     "val_is_x": 0.6,
     "min_val": -5,
     "max_val": 5,
-    "max_depth": 2,
-    "is_real": False,
+    "max_depth": 5,
+    "is_real": True,
     "start_depth": 0,
     "variables": VARS,
 }
@@ -27,6 +27,7 @@ inputs = np.array(dataset2[["x1", "x2", "x3"]])
 # Scale x1 and x2 down
 inputs[:, 0] = np.interp(inputs[:, 0], (inputs[:, 0].min(), inputs[:, 0].max()), (-1, +1))
 inputs[:, 1] = np.interp(inputs[:, 1], (inputs[:, 1].min(), inputs[:, 1].max()), (-1, +1))
+inputs[:, 2] = np.interp(inputs[:, 2], (inputs[:, 2].min(), inputs[:, 2].max()), (-1, +1))
 
 outputs = np.array(dataset2["y"])
 x_train, x_test, y_train, y_test = train_test_split(inputs, outputs, test_size = 0.25)
@@ -34,11 +35,13 @@ x_train, x_test, y_train, y_test = train_test_split(inputs, outputs, test_size =
 def initialize(size, params):
     return [Equation(params) for i in range(size)]
 
-NUM_GENERATIONS = 20
+NUM_GENERATIONS = 5
 SIZE = 100
-MUTATION_PROB = 0.3
-CROSSOVER_PROB = 0.4
-PARSIMONY = 1
+MUTATION_PROB = 0.2
+CROSSOVER_PROB = 0.5
+PARSIMONY = 4
+NUM_CONTESTANTS = 4
+RANDOM_INJECTION = 0.1
 
 current_gen = initialize(SIZE, new_eqn_params)
 best_in_each_gen = []
@@ -62,16 +65,25 @@ for t in tqdm(range(NUM_GENERATIONS)):
             next_gen.append(parent1.mutate(new_eqn_params))
         elif flip <= CROSSOVER_PROB + MUTATION_PROB:  # crossover
             parent2 = random.choices(current_gen, weights)[0]
-            #next_gen.extend(parent1.crossover(parent2))
-            next_gen.append(parent1.crossover(parent2)[0])
+            next_gen.extend(parent1.crossover(parent2))
+            # next_gen.append(parent1.crossover(parent2))
         else:  # clone
             next_gen.append(parent1)
 
-    # # Perform the selection tournament
-    # for j in range(SIZE):
+    # Perform the selection tournament
+    # TODO: it's currently based off of only MSE, maybe fix to include regularization
+    selected = []
+    for j in range(SIZE):
+        if j < SIZE * RANDOM_INJECTION:
+            new_rand_eqn = Equation(new_eqn_params)
+            new_rand_eqn.set_MSE(x_train, y_train, variables = VARS)
+            selected.append(new_rand_eqn)
 
+        else:
+            contestants = random.choices(next_gen, k=NUM_CONTESTANTS)
+            selected.append(min(contestants, key=lambda t: t.MSE))
 
-    current_gen = next_gen   
+    current_gen = selected   
 
     best_eqn = min(current_gen, key=lambda t: t.MSE)
 
